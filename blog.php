57d4a5f9b4e60e6cef778325106135321168d735
zeknogueira <emelonogueira35@gmail.com>
2023-09-07 12:35:21 -0300

    Adicionado arquivo -blog.php- e edições no css da página.

---
+++ blog.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Zek Nogueira</title>
+</head>
+<body>
+    <header>
+        <h1>Zek Nogueira Portifólio</h1>
+        <a href="blog.html">Blog</a>
+    </header>
+    <main>
+        <h1>Artigos</h1>
+
+    </main>
+    <footer>
+        <p>Desenvolvido por <a href="instagram.com/zeknogueira">Zek Nogueira</a></p>
+    </footer>
+    
+</body>
+</html>
