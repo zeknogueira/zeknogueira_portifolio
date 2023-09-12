d2308993ddfc49f65ca5cef9818e41c92d32afca
Ezequiel Melo Nogueira <110491770+zeknogueira@users.noreply.github.com>
2023-09-12 17:23:18 -0300

    Preparing to conection the database

--- blog.php
+++ blog.php
@@ -6,17 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/blog.css">
     <title>Zek Nogueira</title>
 </head>
 
-<body>
-    <header>
-        <div id="logo">
-            <h1>ZEK</h1>
+<body class="white_body_blog">
+    <header id="small_header">
+        <div id="logo_blog">
+            <h1>ZEK<span id="zek_logo_blog">/Blog</span></h1>
         </div>
         <div id="menu">
             <nav id="principal_nav">
-                <a href="blog.php">Blog</a>
+                <a href="blog.html">Blog</a>
                 <a href="contato.html">Contato</a>
                 <a href="Projetos">Projetos</a>
                 <a href="curriculo.html">Curriculo</a>
@@ -24,11 +25,21 @@
         </div>
 
     </header>
-    <main>
-        <div id="principal">
-            <p id="my_name_is">Olá, meu nome é</p>
-            <h1 id="name">Zek Nogueira</h1>
-            <h2 id="dev_web">Desenvolvedor Web</h2>
+    <main id="white_main_blog">
+        <div id="title_articles_box">
+        <h1 id="article_title_blog">Artigos</h1>
+        </div>
+        <div id="main_blog_principal_box">
+        <div id="main_blog_principal">
+            <section  id="articles_section">
+                <article class="principal_articles" id="article_1">
+
+                </article>
+                <article class="principal_articles" id="article_2">
+
+                </article>
+            </section>
+        </div>
         </div>
 
     </main>
@@ -36,7 +47,7 @@
         <div>
             <p>2023 &#169; Todos os direitos reservados | Desenvolvido por <a href="index.html">Zek Nogueira</a></p>
         </div>
-        
+
     </footer>
 
 </body>
