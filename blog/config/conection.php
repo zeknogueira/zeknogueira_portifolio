256dd7fa187190dbe2222522f0bd03bc24150918
zeknogueira <emelonogueira35@gmail.com>
2025-03-18 14:25:16 -0300

    organizing files to restart project

--- blog/config/conection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "zek_blog_articles";
-
-$conection_db = new mysqli($servername, $username, $password, $dbname);
-$conection_db->query("SET NAMES 'utf8'");
--- blog/elements/footer_pattern.php
+++
@@ -1,9 +0,0 @@
-<?php ?>
-<footer id="footer_blog_principal">
-    <div>
-        <p>2023 &#169; Todos os direitos reservados | Desenvolvido por <a href="../../index.php">Zek Nogueira</a>
-        </p>
-    </div>
-
-</footer>
-<?php ?>
